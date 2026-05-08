0a878761b94c06ad61b461c7e75ef69e619c1297
hannanfeisal80-tech <hannanfeisal80@gmail.com>
2026-05-08 14:19:27 +0300

    sign up form

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ console.log(mainHeading);
 
 const introParagraph = document.querySelector(".intro");
 console.log(introParagraph);
+
+const allParagraphs = document.querySelector(".into");
+console.log(allParagraphs);
+
+mainHeading.textContent = "hello javascript in dom"
+mainHeading.style.color = "Green";
